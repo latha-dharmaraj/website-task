5fc54320981316f51946fc7d03dca02d5720a1d3
lathadharmaraj <lathadharmaraj24@gmail.com>
2026-01-12 20:00:07 +0530

    added navigation to mobile

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ $(document).ready(function () {
             settings: {
                 slidesToShow: 1,
                 slidesToScroll: 1,
+                arrows: false
             }
         }]
     });
