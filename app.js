f9703f469e78d7e5699d5e6612f164de2b8bd57c
lathadharmaraj <lathadharmaraj24@gmail.com>
2026-01-12 11:55:08 +0530

    wrote js code for go to

--- app.js
+++ app.js
@@ -1,9 +1,4 @@
-// document.querySelector(".fa-bars").addEventListener("click", function () {
-//     document.querySelector(".side-nav-bar").classList.add("nav-open");
-// });
-// document.querySelector(".side-nav h3").addEventListener("click", function () {
-//     document.querySelector(".side-nav-bar").classList.remove("nav-open");
-// });
+
 
 
 document.querySelector(".fa-bars").addEventListener("click", function () {
@@ -12,10 +7,15 @@ document.querySelector(".fa-bars").addEventListener("click", function () {
 document.querySelector(".side-nav h3").addEventListener("click", function(){
     document.querySelector(".side-nav-bar").classList.remove("nav-open");
 });
-document.querySelector(".side-nav p").addEventListener("click", function(){
-    document.querySelector(".side-nav").window.open("./pages/dental-clinic.html");
-})
 
+// document.querySelector(".side-nav p").addEventListener("click", function(){
+//     document.querySelector(".side-nav").window.open("./pages/dental-clinic.html");
+// })
+
+let dentalClinicPage = document.querySelector(".dental-clinic-page");
+dentalClinicPage.innerHTML = '<a href="pages/dental-clinic.html" class="dental-clinic-page-one" method="post">Dental Clinic Page</a>';
+document.querySelector(".dental-clinic-page-one").style.color = "white";
+document.querySelector(".dental-clinic-page-one").style.textDecoration = "none";
 
 $(document).ready(function () {
     $('.learn-cooking-img-sec-container').slick({
